93ddcd2a3e01b7df949f0b119e4de6ce47e9b1a0
Tommy Coleman <tommiilee@gmail.com>
2020-10-09 12:11:24 -0400

    wip - getting headers to work with wow token api

--- auctionBot.py
+++ auctionBot.py
@@ -1,22 +1,47 @@
 import os
 
 import discord
+import requests
 from dotenv import load_dotenv
+from discord.ext import commands
+# from oauth_hook import OAuthHook
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
+WOW_CLIENT = os.getenv('WOW_CLIENT')
+WOW_SECRET = os.getenv('WOW_SECRET')
+token = ''
+@bot.command(name="hello")
+async def hello(message):
+    await message.send(f'Hello {message.author}!')
 
-@client.event
-async def on_ready():
-    for guild in client.guilds:
-        print(guild)
-    print(f'{client.user} has connected to Discord!')
+@bot.command(name="auth")
+async def authorize(message):
+    response = create_access_token(WOW_CLIENT, WOW_SECRET)
+    token = response['access_token']
+    print(response)
+    print(token)
+    await message.send(response)
+@bot.command(name="token")
+async def get_token(message):
+    headers = { 'content-type': 'application/json', 'Authorization' : f'Bearer {token}'}
+    token_price = requests.get(f'https://us.api.blizzard.com/data/wow/token/index?namespace=dynamic-us&locale=en_US', headers = headers)
+    tokenJson = token_price.json()
+    print(tokenJson)
+    await message.send(token_price)
 
-@client.event
-async def on_message(message):
-    if message.content[0] == '!':
-        await message.channel.send(f'Hello {message.author}')
+def create_access_token(client_id, client_secret):
+    data = { 'grant_type': 'client_credentials' }
+    response = requests.post('https://us.battle.net/oauth/token', data=data, auth=(client_id, client_secret))
+    return response.json()
 
-client.run(TOKEN)
+response = create_access_token(WOW_CLIENT, WOW_SECRET)
+print(response)
+
+
+
+
+
+bot.run(TOKEN)
