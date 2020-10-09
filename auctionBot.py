e669c08e9b8d78080c7718d3fd28f4d6acf834b4
Tommy Coleman <tommiilee@gmail.com>
2020-10-09 16:24:38 -0400

    auction api now functioning

--- auctionBot.py
+++ auctionBot.py
@@ -20,7 +20,19 @@ def create_access_token(client_id, client_secret):
 
 response = create_access_token(WOW_CLIENT, WOW_SECRET)
 token = response['access_token']
+headers = { 'content-type': 'application/json;charset=UTF-8','Authorization' : f'Bearer {token}'}
 
+async def item_search(item, search_item):      
+    items = requests.get(f'https://us.api.blizzard.com/data/wow/search/item?namespace=static-us&locale=en_US&name.en_US={item}&orderby=id', headers = headers)
+   
+    items = items.json()
+    items = items['results']
+    items_dict = {}
+    for i in items:
+        name = i['data']['name']['en_US']
+        if name.find(search_item) != -1:
+            items_dict[i['data']['id']] = { 'name' : i['data']['name']['en_US'], 'price' : 'Not in AH'}
+    return items_dict
 
 @bot.command(name="hello")
 async def hello(message):
@@ -38,24 +50,43 @@ async def get_token(message):
     user = user[0]
     await message.send(f'Hello {user}, the current price of a token is {price} gold')
 
-@bot.command(name="search")
-async def item_search(message):
-    headers = { 'content-type': 'application/json;charset=UTF-8','Authorization' : f'Bearer {token}'}
+
+
+@bot.command(name="price")
+async def price_check(message):
     item = str(message.message.content)
+    print(item)
+    search_item = item[7:]
     item = item.split(' ')
-    item = item[-1]
-        
-    items = requests.get(f'https://us.api.blizzard.com/data/wow/search/item?namespace=static-us&locale=en_US&name.en_US={item}&orderby=id', headers = headers)
-    # items = items.text
-#    items = items['results']
-    # items = items.json()
-   
-    items = items.json()
-    items = items['results']
-    items = [i['data']['name']['en_US'] for i in items]
-    await message.send(str(items))
+    item = item[1]
+    items = await item_search(item, search_item)
+    auctions = requests.get(f'https://us.api.blizzard.com/data/wow/connected-realm/60/auctions?namespace=dynamic-us&locale=en_US', headers = headers)
+    print(search_item)
+    auctions = auctions.json()
+    auctions = auctions['auctions']
+    for auction in auctions:
+        if auction['item']['id'] in items:
+            if 'buyout' in auction:
+                if auction['buyout'] < 100:
+                    items[auction['item']['id']]['price'] = str(auction['buyout']) + ' copper'
+                elif auction['buyout'] < 10000:
+                    items[auction['item']['id']]['price'] = str(auction['buyout'])[:-2] + ' silver'
+                else:
+                    items[auction['item']['id']]['price'] = str(auction['buyout'])[:-4] + ' gold'
 
-
+            elif 'unit_price' in auction:
+                if auction['unit_price'] < 100:
+                    items[auction['item']['id']]['price'] = str(auction['unit_price']) + ' copper per unit'
+                elif auction['unit_price'] < 10000:
+                    items[auction['item']['id']]['price'] = str(auction['unit_price'])[:-2] + ' silver per unit'
+                else:
+                    items[auction['item']['id']]['price'] = str(auction['unit_price'])[:-4] + ' silver per unit'
+    if len(items.keys()) > 5:
+        await message.send('Too many items containing that name, please be more specific')
+    else:
+        for i in items.keys():
+            await message.send(f'{items[i]["name"]} is {items[i]["price"]}')
+    
 
 
 
