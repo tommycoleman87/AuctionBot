b2ee0fc9355b79ec1b99beb83d035ede28713f54
Tommy Coleman <tommiilee@gmail.com>
2020-10-09 12:58:20 -0400

    wow token api now functional

--- auctionBot.py
+++ auctionBot.py
@@ -12,25 +12,6 @@ GUILD = os.getenv('DISCORD_GUILD')
 bot = commands.Bot(command_prefix='!')
 WOW_CLIENT = os.getenv('WOW_CLIENT')
 WOW_SECRET = os.getenv('WOW_SECRET')
-token = ''
-@bot.command(name="hello")
-async def hello(message):
-    await message.send(f'Hello {message.author}!')
-
-@bot.command(name="auth")
-async def authorize(message):
-    response = create_access_token(WOW_CLIENT, WOW_SECRET)
-    token = response['access_token']
-    print(response)
-    print(token)
-    await message.send(response)
-@bot.command(name="token")
-async def get_token(message):
-    headers = { 'content-type': 'application/json', 'Authorization' : f'Bearer {token}'}
-    token_price = requests.get(f'https://us.api.blizzard.com/data/wow/token/index?namespace=dynamic-us&locale=en_US', headers = headers)
-    tokenJson = token_price.json()
-    print(tokenJson)
-    await message.send(token_price)
 
 def create_access_token(client_id, client_secret):
     data = { 'grant_type': 'client_credentials' }
@@ -38,7 +19,30 @@ def create_access_token(client_id, client_secret):
     return response.json()
 
 response = create_access_token(WOW_CLIENT, WOW_SECRET)
-print(response)
+token = response['access_token']
+
+
+@bot.command(name="hello")
+async def hello(message):
+    await message.send(f'Hello {message.author}!')
+
+@bot.command(name="token")
+async def get_token(message):
+    headers = { 'content-type': 'application/json', 'Authorization' : f'Bearer {token}'}
+    token_price = requests.get(f'https://us.api.blizzard.com/data/wow/token/index?namespace=dynamic-us&locale=en_US', headers = headers)
+    price = token_price.json()
+    price = str(price['price'])[:-4]
+    # price = price[:-4]
+    user = str(message.author)
+    user = user.split('#')
+    user = user[0]
+    await message.send(f'Hello {user}, the current price of a token is {price} gold')
+
+@bot.command(name="search")
+async def item_search(message):
+    print(message.content)
+
+
 
 
 
