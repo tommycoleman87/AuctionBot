9d2710843468245e44b4bfc4feb895dfe58331c4
Tommy Coleman <tommiilee@gmail.com>
2020-10-17 18:59:01 -0400

    wip - converting to class

--- auctionBot_class.py
+++ auctionBot_class.py
@@ -1,19 +1,22 @@
+import os
 import time
 import requests
-class AuctionBot():
-    host = None
-    key = None
-    secret = None
-    access_token = None
-    access_token_expiration = None
+from dotenv import load_dotenv
 
-    def __init__(self,key,secret):
+load_dotenv()
+
+WOW_CLIENT = os.getenv('WOW_CLIENT')
+WOW_SECRET = os.getenv('WOW_SECRET')
+class AuctionBot():
+
+
+    def __init__(self):
         # the function that is executed when
         # an instance of the class is created
         self.host = 'https://us.battle.net/oauth/token'
-        self.key = key
-        self.secret = secret
-
+        self.access_token = None
+        self.expiration = None
+        self.server = {'name': 'Stormrage', 'id': 60}
         try:
             self.access_token = self.getAccessToken()
             if self.access_token is None:
@@ -21,7 +24,7 @@ class AuctionBot():
         except Exception as e:
             print(e)
         else:
-            self.access_token_expiration = time.time() + 86300
+            self.expiration = time.time() + 86300
 
     def getAccessToken(self):
         # the function that is 
@@ -29,9 +32,9 @@ class AuctionBot():
         try:
             # build the JWT and store
             # in the variable `token_body`
-
+            data = { 'grant_type': 'client_credentials' }
             # request an access token
-            request = requests.post(self.host)
+            request = requests.post(self.host, data=data, auth=(WOW_CLIENT, WOW_SECRET))
 
             # optional: raise exception for status code
             request.raise_for_status()
@@ -42,6 +45,10 @@ class AuctionBot():
             # assuming the response's structure is
             # {"access_token": ""}
             return request.json()['access_token']
+
+    async def greet(self, message):
+        author = message.author.split('#')[0]
+        await message.send(f'Greetings {author}')
 
     class Decorators():
         @staticmethod
